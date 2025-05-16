ec759f760692fb82e0590745c92f326f073b7b1f
merzouka <marzoukayouness@gmail.com>
2025-05-16 09:06:06 +0100

    Added up script

---
+++ challenges/up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONFIG_DIR="./ready/"
+for category in $(ls $CONFIG_DIR); do
+    for name in $(ls "$CONFIG_DIR/$category"); do
+        if [ -f "$CONFIG_DIR/$category/$name/compose.yaml" ]; then
+        docker compose -f "$CONFIG_DIR/$category/$name/compose.yaml" up -d
+        fi
+    done
+done
