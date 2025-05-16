1c56042ab846390b8317d1eded00d0abf3d90eec
merzouka <marzoukayouness@gmail.com>
2025-05-17 00:10:52 +0100

    Added award calculation script

---
+++ scripts/decay/calc.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+day1 = "16-05-2025"
+day2 = "17-05-2025"
+phases = [
+    [f"{day1}T20:00", f"{day1}T21:30"],
+    [f"{day1}T22:00", f"{day2}T7:30"],
+    [f"{day2}T8:30", f"{day2}T12:00"],
+    [f"{day2}T13:30", f"{day2}T20:00"],
+]
+
+# decay by 1/9 of the value
+value = float(input("Challenge value (total points): "))
+award = float(input("Player score: "))
+
+for phase in phases:
+    now = datetime.now()
+    start = datetime.strptime(phase[0], "%d-%m-%YT%H:%M")
+    end = datetime.strptime(phase[1], "%d-%m-%YT%H:%M")
+    total_diff = abs(end - start)
+    if  now >= start  and now <= end:
+        diff = abs(now - start)
+        delay = min((total_diff / 3), timedelta(hours=2))
+        if diff <= delay:
+            print(f"Calculated Award: {award}")
+            break
+
+        diff = diff - delay
+        print(diff)
+        new_value = max(value / 3, value - (((diff // timedelta(hours=1)) / 12) * value))
+        print(f"Current challenge value: {new_value}")
+        calculated_award = (award / value) * new_value
+        print(f"Calculated award: {calculated_award}")
