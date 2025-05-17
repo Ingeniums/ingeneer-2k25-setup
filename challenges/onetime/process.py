f0d0576bba334b1a704e7456523e309c72bf7482
merzouka <marzoukayouness@gmail.com>
2025-05-17 18:23:26 +0100

    Fixed update script, change challenges to be dynamic

--- challenges/onetime/process.py
+++ challenges/onetime/process.py
@@ -6,33 +6,26 @@ import sys
 def update_yaml_with_files(yaml_file, dir_path):
     files_dir = os.path.join(dir_path, 'files')
     
-    try:
-        with open(yaml_file, 'r') as f:
-            data = yaml.safe_load(f) or {}
-    except FileNotFoundError:
-        data = {}
+    with open(yaml_file, 'r') as f:
+        data = yaml.safe_load(f) or {}
     
-    tags = list(data['tags'])
-    difficulty = ""
-    for tag in tags:
-        if tag in ["warmup", "easy", "medium", "hard", "tough"]:
-            difficulty = tag
-            break
-    if os.path.exists(files_dir):
-        file_paths = [
-            os.path.join(dir_path, 'files', f) 
-            for f in os.listdir(files_dir) 
-            if os.path.isfile(os.path.join(files_dir, f))
-        ]
-        data['files'] = file_paths
+    if data["category"] not in ["design"]:
+        data["extra"] = {
+            "initial": "{{initial}}",
+            "decay": "{{decay}}",
+            "minimum": "{{minimum}}",
+        }
+        del data["value"]
+        data["type"] = "dynamic"
+        tags = []
+        for tag in list(data["tags"]):
+            if tag not in ["warmup", "easy", "medium", "hard", "tough"]:
+                tags.append(tag)
 
-    data['type'] = "dynamic"
-    data['extra'] = {
-        "initial": f"{{{{{difficulty}_initial}}}}",
-        "decay": f"{{{{{difficulty}_decay}}}}",
-        "minimum": f"{{{{{difficulty}_minimum}}}}",
-    }
-    del data["value"]
+        data["tags"] = tags
+    else:
+        data["type"] = "standard"
+
     
     with open(yaml_file, 'w') as f:
         yaml.safe_dump(data, f, default_flow_style=False)
